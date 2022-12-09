cd8960de4c8a21dbec0e8d859daa9f5bf7c43a88
MatiasDevop <solomatias321@gmail.com>
2022-12-09 14:13:43 +0100

    implementing test with mock

---
+++ Product.Backend.TestApi/MockData/ProductMockData.cs
@@ -0,0 +1,39 @@
+﻿using Product.Backend.Domain;
+
+namespace Product.Backend.TestApi.MockData
+{
+    public class ProductMockData
+    {
+        public static List<ProductEntity> GetProducts()
+        {
+            return new List<ProductEntity> {
+                new ProductEntity {
+                    ProductId = new Guid("a63797fa-1c14-438c-8df9-7a07d83091ed"),
+                    Name = "DC",
+                    Code = "ffff2",
+                    Description = "Movies",
+                    IsActive = true
+                },
+                new ProductEntity {
+                    ProductId = new Guid("3addc985-b9c5-4620-bfe1-16ad88e383c1"),
+                    Name = "item2",
+                    Code = "123d",
+                    Description = "electronics",
+                    IsActive = true
+                },
+                new ProductEntity {
+                    ProductId = new Guid("387f127c-c6a7-461b-a836-2d9078bc5bdb"),
+                    Name = "item3",
+                    Code = "x3x3",
+                    Description = "electronics",
+                    IsActive = true
+                }
+            };
+        }
+
+        public static List<ProductEntity> EmptyList()
+        {
+            return new List<ProductEntity>();
+        }
+    }
+}
---
+++ Product.Backend.TestApi/Systems/Controllers/TestProductController.cs
@@ -0,0 +1,46 @@
+﻿
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Product.Backend.Api.Controllers;
+using Product.Backend.Application.Product;
+using Product.Backend.TestApi.MockData;
+
+namespace Product.Backend.TestApi.Systems.Controllers
+{
+    public class TestProductController
+    {
+        [Fact]
+        public async Task GetAllASync_ShouldReturn200Status()
+        {
+            //Arrange
+            var proService = new Mock<IProductService>();
+            proService.Setup(x => x.GetAllAsync()).ReturnsAsync(ProductMockData.GetProducts()); //
+            //sut = system under test
+            var sut = new ProductController(proService.Object);
+            //Act
+            var result = await sut.GetAllAsync();
+
+            ///Assert
+            result.GetType().Should().Be(typeof(OkObjectResult));
+            (result as OkObjectResult).StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public async Task GetAllASync_ShouldReturnStatus204()
+        {
+            //Arrange
+            var proService = new Mock<IProductService>();
+            proService.Setup(x => x.GetAllAsync()).ReturnsAsync(ProductMockData.EmptyList()); //
+            //sut = system under test
+            var sut = new ProductController(proService.Object);
+
+            //Act
+            var result = await sut.GetAllAsync();
+
+            ///Assert
+            result.GetType().Should().Be(typeof(NoContentResult));
+            (result as NoContentResult).StatusCode.Should().Be(204);
+        }
+    }
+}
