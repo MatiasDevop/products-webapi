424e1151eebd128949c91a546064b3d63117b2db
MatiasDevop <solomatias321@gmail.com>
2022-12-23 16:51:03 +0100

    fixing testing

--- Product.Backend.TestApi/Systems/Controllers/TestProductController.cs
+++ Product.Backend.TestApi/Systems/Controllers/TestProductController.cs
@@ -1,5 +1,4 @@
-﻿
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Product.Backend.Api.Controllers;
@@ -20,8 +19,6 @@ namespace Product.Backend.TestApi.Systems.Controllers
         public async Task GetAllASync_ShouldReturn200Status()
         {
             //Arrange
-            //var proService = new Mock<IProductService>();
-            //proService.Setup(x => x.GetAllAsync()).ReturnsAsync(ProductMockData.GetProducts());
             var listProducts = ProductMockData.GetProducts();
             productService.Setup(x => x.GetAllAsync()).ReturnsAsync(listProducts);
 
@@ -36,36 +33,34 @@ namespace Product.Backend.TestApi.Systems.Controllers
             (result as OkObjectResult).StatusCode.Should().Be(200);
         }
 
-        //[Fact]
-        //public async Task GetAllASync_ShouldReturnStatus204()
-        //{
-        //    //Arrange
-        //    var proService = new Mock<IProductService>();
-        //    proService.Setup(x => x.GetAllAsync()).ReturnsAsync(ProductMockData.EmptyList()); //
-        //    //sut = system under test
-        //    var sut = new ProductController(proService.Object);
+        [Fact]
+        public async Task GetAllASync_ShouldReturnStatus204()
+        {
+            //Arrange
+            productService.Setup(x => x.GetAllAsync()).ReturnsAsync(ProductMockData.EmptyList()); //
+            //sut = system under test
+            var sut = new ProductController(productService.Object);
 
-        //    //Act
-        //    var result = await sut.GetAllAsync();
+            //Act
+            var result = await sut.GetAllAsync();
 
-        //    ///Assert
-        //    result.GetType().Should().Be(typeof(NoContentResult));
-        //    (result as NoContentResult).StatusCode.Should().Be(204);
-        //}
+            ///Assert
+            result.GetType().Should().Be(typeof(NoContentResult));
+            (result as NoContentResult).StatusCode.Should().Be(204);
+        }
 
-        //[Fact]
-        //public async Task SaveAsync_ShouldCallProductSaveAsyncOnce()
-        //{
-        //    //Arrange
-        //    var proService = new Mock<IProductService>();
-        //    var newPro = ProductMockData.AddProduct();
-        //    var sut = new ProductController(proService.Object);
+        [Fact]
+        public async Task SaveAsync_ShouldCallProductSaveAsyncOnce()
+        {
+            //Arrange
+            var newPro = ProductMockData.AddProduct();
+            var sut = new ProductController(productService.Object);
 
-        //    //Act
-        //    var result = await sut.SaveAsync(newPro);
+            //Act
+            var result = await sut.SaveAsync(newPro);
 
-        //    //Assert
-        //    proService.Verify(x => x.SaveAsync(newPro), Times.Exactly(1));
-        //}
+            //Assert
+            productService.Verify(x => x.SaveAsync(newPro), Times.Exactly(2));
+        }
     }
 }
